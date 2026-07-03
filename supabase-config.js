e31ab496a24efad23884ee0a3fb72eba5f3cfd28
PJKaraffa <61504379+PJKaraffa@users.noreply.github.com>
2026-07-03 15:05:08 -0400

    Simplify supabaseClient configuration
    Removed unnecessary configuration options for supabaseClient.

--- supabase-config.js
+++ supabase-config.js
@@ -3,13 +3,5 @@ const SUPABASE_KEY = "sb_publishable_Y5_yifLRZTKIF1HO-EfSdQ_K19NxATf";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
-  SUPABASE_KEY,
-  {
-    auth: {
-      persistSession: true,
-      storage: window.sessionStorage,
-      autoRefreshToken: true,
-      detectSessionInUrl: true
-    }
-  }
+  SUPABASE_KEY
 );
